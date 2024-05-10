da2048e52f36886850dd5a9c60859da5a2df79ce
yzg <17635700409@163.com>
2024-05-10 10:17:30 +0800

    feat 🙌

---
+++ src/views/home/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+import {ref} from "vue";
+
+const keyword = ref('qy')
+</script>
+
+<template>
+  <div>
+    <header class="w-full p-[10px]">
+      <el-input v-model="keyword" class="h-[60px] !border-0" placeholder="请输入"/>
+    </header>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
