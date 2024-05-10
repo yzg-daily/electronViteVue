e771499ce817d5443be623a884c3e1804db35261
yzg <17635700409@163.com>
2024-05-10 10:16:22 +0800

    feat router

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
+import router from "./router/index";
 
-import './style.css'
+import './css/index.css'
+
 
 import './demos/ipc'
+
 // If you want use Node.js, the`nodeIntegration` needs to be enabled in the Main process.
 // import './demos/node'
+const app = createApp(App);
 
-createApp(App)
-  .mount('#app')
-  .$nextTick(() => {
-    postMessage({ payload: 'removeLoading' }, '*')
-  })
+app.use(ElementPlus)
+app.use(router)
+app.mount('#app')
+    .$nextTick(() => {
+        postMessage({payload: 'removeLoading'}, '*')
+    })
